b643070a9795af51887328ceb0c33890046298c8
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2026-01-07 10:56:47 +0000

    format code

--- main.c
+++ main.c
@@ -2,44 +2,45 @@
 #include <curl/curl.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     CURL *curl;
     CURLcode res;
     FILE *devnull;
-    
+
     if(argc < 3) {
         printf("Usage: %s <URL> <NUMBER>\n", argv[0]);
         return 1;
     }
-    
+
     devnull = fopen("/dev/null", "w");
     if(!devnull) {
         perror("Failed to open /dev/null");
         return 1;
     }
-    
+
     int success = 0;
     int error = 0;
-    for(int i = 0; i < atoi(argv[2]); i++) { 
+    for(int i = 0; i < atoi(argv[2]); i++) {
         curl = curl_easy_init();
         if(curl) {
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
             curl_easy_setopt(curl, CURLOPT_WRITEDATA, devnull);
             curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
-            
+
             res = curl_easy_perform(curl);
-            
+
             if(res != CURLE_OK) {
                 fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
                 error++;
             } else {
                 success++;
             }
-            
+
             curl_easy_cleanup(curl);
         }
-    } 
+    }
     printf("ALL:     %d\n",atoi(argv[2]));
     printf("ERROR:   %d\n",error);
     printf("SUCCESS: %d\n",success);
